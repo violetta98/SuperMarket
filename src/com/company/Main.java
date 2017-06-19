bfb851f42ea3bf42f429c48f7446f1182109c5bc
Violetta Podvolotskaya <violettapidvolotska@gmail.com>
2017-06-19 21:06:44 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,65 @@
 package com.company;
 
 //import com.company.entity.cashiers.RandomizedCashier;
+import com.company.entity.Cashier;
 import com.company.entity.Customer;
 import com.company.entity.cashiers.LazyCashier;
 import com.company.entity.cashiers.NormalCashier;
-import com.company.entity.customers.RandomizedCustomer;
+import com.company.entity.customers.RandomizedAgressiveCustomer;
+import com.company.entity.customers.RandomizedLazyCustomer;
+import com.company.entity.customers.RandomizedNormalCustomer;
 import com.company.entity.Register;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 public class Main {
-    public static void main(String[] args) {
-//        String string="jfdkjgi";
-//        System.out.println(string.getClass());
-        Register register = new Register(new LazyCashier());
-        register.newCustomer(new AnotherCustomer());
-        register.newCustomer();
-        register.run();
+    public static void main(String[] args) throws InterruptedException {
+        Main m = new Main();
+        for (int i=0; i<100000; i++)
+             new Thread(m.newRegister()).start();
+//        ExecutorService pool = Executors.newCachedThreadPool(new ThreadFactory() {
+//            @Override
+//            public Thread newThread(Runnable r) {
+//                Thread thread = new Thread(r);
+//                thread.setName("lol");
+//                thread.setPriority(342);
+//                return thread;
+//            }
+//        });
+        ExecutorService pool = Executors.newFixedThreadPool(5); // надо остановить executor
+        for (int i=0; i<5; i++) {
+            pool.submit(m.newRegister());
+        }
+        pool.shutdown();
+        // register.run();
+        // Thread.currentThread().sleep(1);
+        System.out.println("all done");
     }
 
-    private static Customer randomCustomer() {
-        if (new Random.nextBoolean())
-            return new RandomizedCustomer();
+    private Cashier randomCashier() {
+       if (new Random().nextBoolean())
+           return new LazyCashier(); // садим в кассу ленивого кассира
+       else
+           return new NormalCashier(); // садим в кассу нормального кассира
+    }
+
+    private Customer randomCustomer() {
+        int q=new Random().nextInt(3);
+        if (q==0)
+            return new RandomizedNormalCustomer(); // добавляем нормального покупателя в очередь
+        else if (q==1)
+            return new RandomizedLazyCustomer();  // добавляем ленивого покупателя в очередь
         else
-            return new AnotherCustomer();
+            return new RandomizedAgressiveCustomer();
+    }
+
+    private Register newRegister() {
+        Register register = new Register(randomCashier()); // садим в кассу рандомного кассира
+        for (int i=0; i<new Random().nextInt(15); i++)
+            register.newCustomer(randomCustomer()); // добавляет в очередь рандомного покупателя
+        return register;
     }
 }
